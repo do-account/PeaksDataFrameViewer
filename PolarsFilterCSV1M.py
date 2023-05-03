03a62f0ea5117775716f6ad0a11307f7c504371e
Advanced Acccounting Automation <124856158+do-account@users.noreply.github.com>
2023-05-03 17:30:17 +0800

    Update PolarsFilterCSV1M.py

--- PolarsFilterCSV1M.py
+++ PolarsFilterCSV1M.py
@@ -5,7 +5,7 @@ import pathlib
 s = time.time()
 
 # read csv file
-df = pl.scan_csv('Input/1MillionRows.csv')
+df = pl.scan_csv('D:/Benchmark/uSpace/1MillionRows.csv')
 filter = df.filter((((pl.col('Ledger') >= "L10") & (pl.col('Ledger') <= "L15")) | ((pl.col('Ledger') >= "L50") & (pl.col('Ledger') <= "L55")) | ((pl.col('Ledger') >= "L82") & (pl.col('Ledger') <= "L88"))) & (((pl.col('Account') >= 12222) & (pl.col('Account') <= 12888)) | ((pl.col('Account') >= 15555) & (pl.col('Account') <= 16888))) & ((pl.col('Project') > "B28") | (pl.col('Project') < "B22")))
 
 # write csv file
